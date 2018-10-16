260b993c8c03da9dfbd4f54aada9a0ed2538ee5c
Jamison Judd <jamison.judd@gmail.com>
2018-10-16 16:45:11 -0400

    updated scripts

--- sbatch_proc.sh
+++ sbatch_proc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --mail-type=ALL 			# Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=First.Last@uconn.edu	# Your email address
+#SBATCH --mail-user=jamison.judd+hpc@uconn.edu	# Your email address
 #SBATCH --ntasks=1				# Run a single serial task
 #SBATCH --cpus-per-task=1     # Number of cores to use
 #SBATCH --mem=8096mb				# Memory limit
-#SBATCH --time=2:00:00				# Time limit hh:mm:ss
+#SBATCH --time=02:00:00				# Time limit hh:mm:ss
 #SBATCH -e error_afni.log				# Standard error
 #SBATCH -o output_afni.log				# Standard output
 #SBATCH --job-name=afni			# Descriptive job name
@@ -13,6 +13,6 @@ module load singularity
 
 
 # run the generated script
-/scratch/psyc5171/NETID
-singularity run /scratch/psyc5171/containers/burc-lite.img /scratch/psyc5171/NETID/run_afni.sh
+cd /scratch/psyc5171/jej00007/hw7
+singularity run /scratch/psyc5171/containers/burc-lite.img /scratch/psyc5171/jej00007/hw7/run_afni.sh
 
